481f4b367fe5b96de9a6e5852be9612d72858cc9
Idan Shemesh <idan.shmsh@gmail.com>
2025-06-16 14:35:05 +0300

    reordered file, fixed exceptions

--- integration/unity/_infra/FieldForgeDedicatedBuffersConfig.cs
+++ integration/unity/_infra/FieldForgeDedicatedBuffersConfig.cs
@@ -12,21 +12,9 @@ namespace FieldForge
             public Func<SimulationData, int> SizeCalculator;
         }
 
-        public static readonly Dictionary<string, BufferDeclaration[]> ShaderDedicatedBuffers = new()
-        {
-            ["prepare_rendering-bloom_preparation-add_fermion_field_norms"] = BLOOM_BUFFER_DECLARATIONS,
-            ["prepare_rendering-bloom_preparation-add_fermion_field_phases"] = BLOOM_BUFFER_DECLARATIONS,
-            ["prepare_rendering-bloom_preparation-load_fermion_field_norms"] = BLOOM_BUFFER_DECLARATIONS,
-            ["prepare_rendering-bloom_preparation-load_fermion_field_phases"] = BLOOM_BUFFER_DECLARATIONS,
-            ["prepare_rendering-bloom_preparation-bloom3"] = BLOOM_BUFFER_DECLARATIONS,
-            ["prepare_rendering-bloom_preparation-bloom6"] = BLOOM_BUFFER_DECLARATIONS,
-            ["prepare_rendering-bloom_preparation-bloom9"] = BLOOM_BUFFER_DECLARATIONS,
-            ["prepare_rendering-bloom_preparation-bloom12"] = BLOOM_BUFFER_DECLARATIONS,
-            ["prepare_rendering-bloom_preparation-bloom15"] = BLOOM_BUFFER_DECLARATIONS,
-            ["Custom/bloom_rendering_2d"] = BLOOM_BUFFER_DECLARATIONS
-        };
+        // =========================================================================================================
 
-        private static BufferDeclaration[] BLOOM_BUFFER_DECLARATIONS = new[]
+        private static readonly BufferDeclaration[] BLOOM_BUFFER_DECLARATIONS = new[]
         {
             new BufferDeclaration
             {
@@ -40,6 +28,22 @@ namespace FieldForge
                 DataType = typeof(int),
                 SizeCalculator = data => (int)(Math.Ceiling(data.simulationWidth / 4.0f) * Math.Ceiling(data.simulationHeight / 4.0f) * Math.Ceiling(data.simulationDepth / 4.0f) * 3) // x0.25x0.25x0.25 the lattice size, x3 rgb channels
             }
+        };
+
+        // =========================================================================================================
+
+        public static readonly Dictionary<string, BufferDeclaration[]> ShaderDedicatedBuffers = new()
+        {
+            ["prepare_rendering-bloom_preparation-add_fermion_field_norms"] = BLOOM_BUFFER_DECLARATIONS,
+            ["prepare_rendering-bloom_preparation-add_fermion_field_phases"] = BLOOM_BUFFER_DECLARATIONS,
+            ["prepare_rendering-bloom_preparation-load_fermion_field_norms"] = BLOOM_BUFFER_DECLARATIONS,
+            ["prepare_rendering-bloom_preparation-load_fermion_field_phases"] = BLOOM_BUFFER_DECLARATIONS,
+            ["prepare_rendering-bloom_preparation-bloom3"] = BLOOM_BUFFER_DECLARATIONS,
+            ["prepare_rendering-bloom_preparation-bloom6"] = BLOOM_BUFFER_DECLARATIONS,
+            ["prepare_rendering-bloom_preparation-bloom9"] = BLOOM_BUFFER_DECLARATIONS,
+            ["prepare_rendering-bloom_preparation-bloom12"] = BLOOM_BUFFER_DECLARATIONS,
+            ["prepare_rendering-bloom_preparation-bloom15"] = BLOOM_BUFFER_DECLARATIONS,
+            ["Custom/bloom_rendering_2d"] = BLOOM_BUFFER_DECLARATIONS
         };
     }
 }
