cec6965e4c7e80d13af7a8f64c53bbaacdf488e6
IdanShmsh <idan.shmsh@gmail.com>
2025-11-05 20:02:46 +0200

    made sure touches don't have the "always poke a stationary poke on touch start" behavior

--- integration/unity/scripts/FieldForgeTouchPoking2D.cs
+++ integration/unity/scripts/FieldForgeTouchPoking2D.cs
@@ -34,6 +34,7 @@ namespace FieldForge
             _touchTracker.Update();
             foreach (Touch touch in Input.touches)
             {
+                if (touch.phase == TouchPhase.Began) continue;
                 if (!_touchTracker.IsTouchActive(touch.fingerId)) continue;
                 if (!(touch.phase == TouchPhase.Began || touch.phase == TouchPhase.Moved)) continue;
                 SubmitCurrentPoke(touch);
