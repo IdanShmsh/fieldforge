3a54ab81b14788cdf80b6e41ec737b6e1d04f800
IdanShmsh <idan.shmsh@gmail.com>
2026-01-09 19:40:51 +0200

    implemented fermion-field curvature (laplacian) computations

--- src/core/analysis/fermion_field_gauge_covariant_wilson_differentials.hlsl
+++ src/core/analysis/fermion_field_gauge_covariant_wilson_differentials.hlsl
@@ -91,6 +91,92 @@ namespace FermionFieldGaugeCovariantWilsonDifferentials
         spatial_derivative(axis, position, field_index, crnt_fermions_lattice_buffer, crnt_gauge_potentials_lattice_buffer, derivative);
     }
 
+    // Take the gauge covariant spatial curvature (second derivative) of a fermion field with a specified field-index, at a specified simulation location,
+    // along a specified spatial axis, in a specified fermion lattice buffer and a specified gauge potentials lattice buffer.
+    // * Side Effects:
+    // • Reads directly from the simulation's lattice buffers.
+    void spatial_curvature(uint axis, half3 position, uint field_index, FermionLatticeBuffer fermion_lattice_buffer, GaugeLatticeBuffer gauge_potentials_lattice_buffer, out FermionFieldState curvature)
+    {
+        FermionFieldStateOps::empty(curvature);
+        if (axis > SPATIAL_DIMENSIONALITY - 1) return;
+        half3 offset = half3(0, 0, 0);
+        offset[axis] = 1;
+        uint lattice_buffer_index;
+        lattice_buffer_index = SimulationDataOps::get_fermion_lattice_buffer_index(position, field_index);
+        FermionFieldState center_fermion_state = fermion_lattice_buffer[lattice_buffer_index];
+        lattice_buffer_index = SimulationDataOps::get_fermion_lattice_buffer_index(position - offset, field_index);
+        FermionFieldState neighboring_fermion_state1 = fermion_lattice_buffer[lattice_buffer_index];
+        lattice_buffer_index = SimulationDataOps::get_fermion_lattice_buffer_index(position + offset, field_index);
+        FermionFieldState neighboring_fermion_state2 = fermion_lattice_buffer[lattice_buffer_index];
+        lattice_buffer_index = SimulationDataOps::get_fermion_lattice_buffer_index(position - offset, field_index ^ 1);
+        FermionFieldState neighboring_weak_partner_state1 = fermion_lattice_buffer[lattice_buffer_index];
+        lattice_buffer_index = SimulationDataOps::get_fermion_lattice_buffer_index(position + offset, field_index ^ 1);
+        FermionFieldState neighboring_weak_partner_state2 = fermion_lattice_buffer[lattice_buffer_index];
+        lattice_buffer_index = SimulationDataOps::get_gauge_lattice_buffer_index(position - offset);
+        GaugeSymmetriesVectorPack link_gauge_potentials1 = gauge_potentials_lattice_buffer[lattice_buffer_index];
+        lattice_buffer_index = SimulationDataOps::get_gauge_lattice_buffer_index(position);
+        GaugeSymmetriesVectorPack link_gauge_potentials2 = gauge_potentials_lattice_buffer[lattice_buffer_index];
+        bool weakDoubletIndex = field_index % 2 == 0;
+        half3 coupling_constants = SimulationDataOps::obtain_fermion_coupling_constants_tuple(field_index);
+        WilsonFormalism::backward_parallel_transport_fermion(
+            neighboring_fermion_state1,
+            neighboring_weak_partner_state1,
+            link_gauge_potentials1,
+            axis + 1,
+            coupling_constants,
+            weakDoubletIndex,
+            neighboring_fermion_state1);
+        WilsonFormalism::parallel_transport_fermion(
+            neighboring_fermion_state2,
+            neighboring_weak_partner_state2,
+            link_gauge_potentials2,
+            axis + 1,
+            coupling_constants,
+            weakDoubletIndex,
+            neighboring_fermion_state2);
+        FermionFieldState sum_of_neighbors;
+        FermionFieldStateMath::sum(neighboring_fermion_state2, neighboring_fermion_state1, sum_of_neighbors);
+        FermionFieldState twice_center;
+        FermionFieldStateMath::rscl(center_fermion_state, 2, twice_center);
+        FermionFieldState second_difference;
+        FermionFieldStateMath::sub(sum_of_neighbors, twice_center, second_difference);
+        FermionFieldStateMath::rscl(second_difference, 1 / (simulation_spatial_unit * simulation_spatial_unit), curvature);
+    }
+
+    // Take the gauge covariant spatial curvature (second derivative) of a fermion field with a specified field-index, at a specified simulation location,
+    // along a specified spatial axis, in the current fermion lattice buffer and the current gauge potentials lattice buffer.
+    // * Side Effects:
+    // • Reads directly from the simulation's lattice buffers.
+    void spatial_curvature(uint axis, half3 position, uint field_index, out FermionFieldState curvature)
+    {
+        spatial_curvature(axis, position, field_index, crnt_fermions_lattice_buffer, crnt_gauge_potentials_lattice_buffer, curvature);
+    }
+
+    // Take the gauge covariant spatial laplacian of a fermion field with a specified field-index, at a specified simulation location,
+    // along all spatial axes, in a specified fermion lattice buffer and a specified gauge potentials lattice buffer.
+    // * Side Effects:
+    // • Reads directly from the simulation's lattice buffers.
+    void spatial_laplacian(half3 position, uint field_index, FermionLatticeBuffer fermion_lattice_buffer, GaugeLatticeBuffer gauge_potentials_lattice_buffer, out FermionFieldState laplacian)
+    {
+        FermionFieldStateOps::empty(laplacian);
+        FermionFieldState axis_curvature;
+        spatial_curvature(0, position, field_index, fermion_lattice_buffer, gauge_potentials_lattice_buffer, axis_curvature);
+        FermionFieldStateMath::sum(laplacian, axis_curvature, laplacian);
+        spatial_curvature(1, position, field_index, fermion_lattice_buffer, gauge_potentials_lattice_buffer, axis_curvature);
+        FermionFieldStateMath::sum(laplacian, axis_curvature, laplacian);
+        spatial_curvature(2, position, field_index, fermion_lattice_buffer, gauge_potentials_lattice_buffer, axis_curvature);
+        FermionFieldStateMath::sum(laplacian, axis_curvature, laplacian);
+    }
+
+    // Take the gauge covariant spatial laplacian of a fermion field with a specified field-index, at a specified simulation location,
+    // along all spatial axes, in the current fermion lattice buffer and the current gauge potentials lattice buffer.
+    // * Side Effects:
+    // • Reads directly from the simulation's lattice buffers.
+    void spatial_laplacian(half3 position, uint field_index, out FermionFieldState laplacian)
+    {
+        spatial_laplacian(position, field_index, crnt_fermions_lattice_buffer, crnt_gauge_potentials_lattice_buffer, laplacian);
+    }
+
     // Take the gauge covariant gradient of a fermion field with a specified field-index, at a specified simulation location,
     // along all spacetime axes, in the current fermion lattice buffer and the current gauge potentials lattice buffer.
     // * Side Effects:
--- src/core/analysis/fermion_field_state_differentials.hlsl
+++ src/core/analysis/fermion_field_state_differentials.hlsl
@@ -52,6 +52,66 @@ namespace FermionFieldStateDifferentials
         spatial_derivative(axis, position, field_index, crnt_fermions_lattice_buffer, field_derivative);
     }
 
+    // Take the spatial curvature (second derivative) of a fermion field with a specified field-index, at a specified simulation location, along a specified spatial axis,
+    // in a specified fermion lattice buffer.
+    // * Side Effects:
+    // • Reads directly from the simulation's lattice buffers.
+    void spatial_curvature(uint axis, half3 position, uint fermion_field_index, FermionLatticeBuffer fermion_lattice_buffer, out FermionFieldState field_curvature)
+    {
+        FermionFieldStateOps::empty(field_curvature);
+        if (axis > SPATIAL_DIMENSIONALITY - 1) return;
+        half3 offset = half3(0, 0, 0);
+        offset[axis] = 1;
+        uint lattice_buffer_index;
+        lattice_buffer_index = SimulationDataOps::get_fermion_lattice_buffer_index(position, fermion_field_index);
+        FermionFieldState center_state = fermion_lattice_buffer[lattice_buffer_index];
+        lattice_buffer_index = SimulationDataOps::get_fermion_lattice_buffer_index(position - offset, fermion_field_index);
+        FermionFieldState neighboring_fermion_state1 = fermion_lattice_buffer[lattice_buffer_index];
+        lattice_buffer_index = SimulationDataOps::get_fermion_lattice_buffer_index(position + offset, fermion_field_index);
+        FermionFieldState neighboring_fermion_state2 = fermion_lattice_buffer[lattice_buffer_index];
+        FermionFieldState sum_of_neighbors;
+        FermionFieldStateMath::sum(neighboring_fermion_state2, neighboring_fermion_state1, sum_of_neighbors);
+        FermionFieldState twice_center;
+        FermionFieldStateMath::rscl(center_state, 2, twice_center);
+        FermionFieldState second_difference;
+        FermionFieldStateMath::sub(sum_of_neighbors, twice_center, second_difference);
+        FermionFieldStateMath::rscl(second_difference, 1 / (simulation_spatial_unit * simulation_spatial_unit), field_curvature);
+    }
+
+    // Take the spatial curvature (second derivative) of a fermion field with a specified field-index, at a specified simulation location, along a specified spatial axis,
+    // in the current fermion lattice buffer.
+    // * Side Effects:
+    // • Reads directly from the simulation's lattice buffers.
+    void spatial_curvature(uint axis, half3 position, uint fermion_field_index, out FermionFieldState field_curvature)
+    {
+        spatial_curvature(axis, position, fermion_field_index, crnt_fermions_lattice_buffer, field_curvature);
+    }
+
+    // Take the spatial laplacian of a fermion field with a specified field-index, at a specified simulation location, along all spatial axes,
+    // in a specified fermion lattice buffer.
+    // * Side Effects:
+    // • Reads directly from the simulation's lattice buffers.
+    void spatial_laplacian(half3 position, uint fermion_field_index, FermionLatticeBuffer fermion_lattice_buffer, out FermionFieldState field_laplacian)
+    {
+        FermionFieldStateOps::empty(field_laplacian);
+        FermionFieldState axis_curvature;
+        spatial_curvature(0, position, fermion_field_index, fermion_lattice_buffer, axis_curvature);
+        FermionFieldStateMath::sum(field_laplacian, axis_curvature, field_laplacian);
+        spatial_curvature(1, position, fermion_field_index, fermion_lattice_buffer, axis_curvature);
+        FermionFieldStateMath::sum(field_laplacian, axis_curvature, field_laplacian);
+        spatial_curvature(2, position, fermion_field_index, fermion_lattice_buffer, axis_curvature);
+        FermionFieldStateMath::sum(field_laplacian, axis_curvature, field_laplacian);
+    }
+
+    // Take the spatial laplacian of a fermion field with a specified field-index, at a specified simulation location, along all spatial axes,
+    // in the current fermion lattice buffer.
+    // * Side Effects:
+    // • Reads directly from the simulation's lattice buffers.
+    void spatial_laplacian(half3 position, uint fermion_field_index, out FermionFieldState field_laplacian)
+    {
+        spatial_laplacian(position, fermion_field_index, crnt_fermions_lattice_buffer, field_laplacian);
+    }
+
     // Take the gradient of a fermion field with a specified field-index, at a specified simulation location, along all spacetime axes.
     // * Side Effects:
     // • Reads directly from the simulation's lattice buffers.
