b4ce6f6524db7fc7b22ff2be29c01375ff3c7d1f
Idan <idan.shmsh@gmail.com>
2026-07-31 18:33:38 +0300

    tweaked fermion norm/phase rendering scaling

--- src/visuals/fermion_renderers/render_fermion_norm_contours.hlsl
+++ src/visuals/fermion_renderers/render_fermion_norm_contours.hlsl
@@ -19,7 +19,7 @@ namespace FermionRendering
             if (!SimulationDataOps::is_fermion_field_active(field_index)) return half4(0, 0, 0, 0);
             FermionFieldState state;
             FieldInterpolations::get_fermion_state_in_position(position, field_index, rend_fermions_lattice_buffer, state);
-            half state_norm = (half)FermionFieldStateMath::norm(state);
+            half state_norm = sqrt((half)FermionFieldStateMath::norm(state));
             half periodic = abs(frac(state_norm * granularity) * 2 - 1) * 2 - 1;
             return periodic * state_norm * (half4)fermion_field_properties[field_index].color;
         }
--- src/visuals/fermion_renderers/render_fermion_phase.hlsl
+++ src/visuals/fermion_renderers/render_fermion_phase.hlsl
@@ -19,9 +19,9 @@ namespace FermionRendering
             if (!SimulationDataOps::is_fermion_field_active(field_index)) return half4(0, 0, 0, 0);
             FermionFieldState state;
             FieldInterpolations::get_fermion_state_in_position(position, field_index, rend_fermions_lattice_buffer, state);
-            half state_norm = (half)FermionFieldStateMath::norm(state);
+            half state_norm_sqrd = (half)FermionFieldStateMath::norm_sqrd(state);
             half state_phase = (half)ComplexNumbersMath::phase(state[0]);
-            half3 hsv = half3(state_phase / (2.0 * 3.14159265), 1.0, state_norm);
+            half3 hsv = half3(state_phase / (2.0 * 3.14159265), 1.0, state_norm_sqrd);
             return half4(CommonMath::hsv2rgb(hsv), 0);
         }
     }
